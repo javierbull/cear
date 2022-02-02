25aee49773adf4b9fda826c582d894b869999a7d
javierbull <javierbullreyes@gmail.com>
2022-02-02 16:56:56 -0500

    base floating labels

--- js/login.js
+++ js/login.js
@@ -15,6 +15,13 @@ $(document).ready(function(){
 });
 // END SHOW-HIDE PASSWORD
 
-
+$('.field__input').on('input', function() {
+  var $field = $(this).closest('.field');
+  if (this.value) {
+    $field.addClass('field--not-empty');
+  } else {
+    $field.removeClass('field--not-empty');
+  }
+});
 
 });
